81b73c0a1cff70f3415ce5dbee85862305073924
Khanh <teteka911@gmail.com>
2026-03-14 20:12:37 +0700

    updte lnk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/KhanhTran-2210"
+                href="https://github.com/KhanhTran-2210/Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
